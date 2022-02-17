337ca250ff684e28f2dd045ac07c438b529e8a2f
egadams <eg1972@gmail.com>
2022-02-16 21:43:26 -0500

    added eslintrc and disabled type check for led class object.

--- src/typescript_source/concrete/ServerLedData.ts
+++ src/typescript_source/concrete/ServerLedData.ts
@@ -6,6 +6,7 @@ import IMonitorLedData from "../abstract/IMonitorLedData";
 class ServerLedData implements IMonitorLedData {
     constructor() { console.log( "constructing ServerLedData..." )}
     ledText = "checking server status...";
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     classObject: any = {
         "background-color": "yellow"
     }
