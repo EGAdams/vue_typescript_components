b243ff095cedb063d59cde79b8115672eac65e97
adamsl <eg1972@gmail.com>
2022-02-20 11:26:03 -0500

    fixed tsconfig error

--- command_manager_monitor/src/CommandManagerMonitor.vue
+++ command_manager_monitor/src/CommandManagerMonitor.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import LogViewer from '@/components/LogViewer.vue';
+import LogViewer from './components/LogViewer.vue';
 export default defineComponent({
     name: 'App',
     components: {
